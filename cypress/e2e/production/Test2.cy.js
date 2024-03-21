117259d7cf4a9c271d8f0712900ec4355564f7ee
Elyas <Elyas575@hotmail.com>
2024-03-21 12:54:17 +0200

    LOL

--- cypress/e2e/production/Test2.cy.js
+++ cypress/e2e/production/Test2.cy.js
@@ -1,13 +1,15 @@
 describe("suite name", () => {
     it("Login and verify home page content", () => {
-        cy.visit("https://app-staging.im-vitro.com/")
+        cy.visit("https://app.im-vitro.com/")
+        // https://app.im-vitro.com/ production
+        // 
 
         // login with (tancho+us+firsttest@im-vitro.com, elias@im-vitro.com)
         cy.get('#email-input').type("elias@im-vitro.com")
         cy.get('#password-input').type("Night-watch11")
         cy.get('button[type="submit"]').click()
         cy.contains('span[data-v-7a7650e0]', 'Log out', { timeout: 20000 }).should('be.visible')
-   
+   // the name will be:
       // verify table content
         cy.get("p.title-1.widget-header", { timeout: 20000 }).should('be.visible').should('have.text','New treatments detected in TLS')
         cy.get(".column-id", { timeout: 20000 }).should('be.visible').should('have.text', 'ID');
